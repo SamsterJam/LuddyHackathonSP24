dfe5e4c975c1a2a8f7037947706c5ef01c59c35a
lillie <onlinechronically@protonmail.com>
2024-04-13 13:18:52 -0400

    remove: menu bar from app

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('node:path')
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    autoHideMenuBar: true
   })
 
   win.loadFile('index.html')
