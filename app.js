459a5431992286ab28526716a629f6ccf24bc43b
lillie <onlinechronically@protonmail.com>
2024-04-13 14:03:14 -0400

    disable: dev tools from keybind

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true
+    autoHideMenuBar: true,
+    webPreferences: {
+      devTools: false
+    }
   })
 
   win.loadFile('index.html')
