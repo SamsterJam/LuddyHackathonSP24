817beb82957869a95e3adff39462b55a5015ba98
lillie <onlinechronically@protonmail.com>
2024-04-13 16:01:13 -0400

    add: pdf slice to ipc runner (client) and remove others

--- views/preload.js
+++ views/preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld("electronAPI", {
-    setInputFile: file => ipcRenderer.invoke("config:setInputFile", file)
+    slicePdf: data => ipcRenderer.invoke("slice-pdf", data)
 })
