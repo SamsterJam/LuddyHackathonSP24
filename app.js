9e80eea8b2aafbf7efa3fca0b127d71cfb717f02
lillie <onlinechronically@protonmail.com>
2024-04-13 20:19:10 -0400

    fix: forgeConfig iterating incorrect amount of times

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function forgeConfig() {
 			
 		}
 	}
-	for (let i = 1; i < config.totalEntries; i++) {
+	for (let i = 1; i <= config.totalEntries; i++) {
 		obj.OCR[`Text${i}`] = config.getEntry(i).text
 		obj.OCR[`Loc${i}`] = [config.getEntry(i).pointOne.x, config.getEntry(i).pointOne.y, config.getEntry(i).pointTwo.x, config.getEntry(i).pointTwo.y]
 	}
